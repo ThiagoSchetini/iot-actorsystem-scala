9fb4ac2cb1f4f425d47e39393da226f0b122de03
thiagoschetini <thiagoschetini@gmail.com>
2019-05-18 18:47:42 -0300

    created the constructor of testSystem

--- src/test/scala/com/example/iot/DeviceSpec.scala
+++ src/test/scala/com/example/iot/DeviceSpec.scala
@@ -2,55 +2,22 @@ package com.example.iot
 
 import akka.actor.ActorSystem
 import akka.testkit.TestKit
-import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec, WordSpecLike}
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
-class DeviceSpec (_system: ActorSystem) extends TestKit(_system) with WordSpecLike with Matchers with BeforeAndAfterAll {
+
+/**
+  * @param testSystem -> define the primary constructor of this class (and of course a val testSystem without getter)
+  *
+  * the unique constructor of TestKit needs an ActorSystem object -> TestKit​(ActorSystem system)
+  * testSystem is passed to the constructor of TestKit -> ... extends TestKit(testSystem)
+  */
+class DeviceSpec (testSystem: ActorSystem) extends TestKit(testSystem)
+  with WordSpecLike with Matchers with BeforeAndAfterAll {
 
   //
-  // creating an actor system and
+  // creating the default constructor
+  // default invokes the defined primary constructor -> class DeviceSpec (testSystem: ActorSystem)...
   //
-  def this() = this(ActorSystem("test-system"))
+  def this() = this(ActorSystem.create("test-system"))
 
-}
-
-
-class Xx (val number: Double) {
-
-
-  this(number:Double)
-}
-
-object Xx extends App {
-  val some = new Xx(33d)
-  println(some)
-}
-
-class ClassAuxiliaryConstructor(private var obrigatoryNumber:Double) {
-
-  var anotherNumber:Int = 12
-  var someString:String = "anything"
-  var someChar:Char = 'a'
-
-  def this(obrigatoryNumber:Double, anotherNumber: Int) {
-    this(obrigatoryNumber)
-    this.anotherNumber = anotherNumber
-  }
-
-  def this(obrigatoryNumber:Double, anotherNumber: Int, someString: String) {
-    this(obrigatoryNumber, anotherNumber)
-    this.someString = someString
-  }
-
-  def this(obrigatoryNumber:Double, anotherNumber: Int, someString: String, someChar: Char) {
-    this(obrigatoryNumber, anotherNumber, someString)
-    this.someChar = someChar
-  }
-
-  override def toString: String =
-    s"""
-       |obrigatory = $obrigatoryNumber
-       |another = $anotherNumber
-       |string = $someString
-       |char = $someChar
-  """.stripMargin
 }
