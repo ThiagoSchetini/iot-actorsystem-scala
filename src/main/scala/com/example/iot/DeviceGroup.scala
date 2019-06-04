59db74a90e499167ac464ab957e2442dd1f9e484
Thiago Schetini Basso <thiago.basso@boavistascpc.com.br>
2019-06-04 10:44:24 -0300

    device group receive refactored to use case match

--- src/main/scala/com/example/iot/DeviceGroup.scala
+++ src/main/scala/com/example/iot/DeviceGroup.scala
@@ -1,7 +1,7 @@
 package com.example.iot
 
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
-import com.example.iot.DeviceManager.{RequestTrackDevice}
+import com.example.iot.DeviceManager.RequestTrackDevice
 
 object DeviceGroup {
   def props(groupId: String): Props = Props(new DeviceGroup(groupId))
@@ -16,15 +16,18 @@ class DeviceGroup(groupId: String) extends Actor with ActorLogging {
   override def receive: Receive = {
 
     // the @ means to store the RequestTrackDevice on trackMsg reference
-    case trackMsg @ RequestTrackDevice(_, `groupId`, deviceId) =>
+    case trackMsg @ RequestTrackDevice(_, `groupId`, _) =>
 
-      if (deviceIdToActor.get(deviceId).isEmpty) {
-        log.info("creating device actor for {}", deviceId)
-        val deviceActor = context.actorOf(Device.props(groupId, deviceId))
-        deviceIdToActor += deviceId -> deviceActor
-        deviceActor.forward(trackMsg)
+      deviceIdToActor.get(trackMsg.deviceId) match {
+        case Some(deviceActor) =>
+          deviceActor.forward(trackMsg)
+
+        case None =>
+          log.info("creating device actor for {}", trackMsg.deviceId)
+          val deviceActor = context.actorOf(Device.props(groupId, trackMsg.deviceId))
+          deviceIdToActor += trackMsg.deviceId -> deviceActor
+          deviceActor.forward(trackMsg)
       }
-      else deviceIdToActor(deviceId).forward(trackMsg)
 
     case RequestTrackDevice(_, groupId, _) => log.warning(
         s"Ignoring TrackDevice Request for group {}. This actor is responsible for group {}",
