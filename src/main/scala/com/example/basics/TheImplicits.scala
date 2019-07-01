838f07298db1a4568486e00a1784aab90b8feced
thiagoschetini <thiagoschetini@gmail.com>
2019-07-01 15:51:51 -0300

    melhorando o estudo de implicits

--- src/main/scala/com/example/basics/TheImplicits.scala
+++ src/main/scala/com/example/basics/TheImplicits.scala
@@ -1,9 +1,6 @@
 package com.example.basics
 
 import java.util.concurrent.{ExecutorService, Executors}
-
-
-
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success}
@@ -11,14 +8,14 @@ import grizzled.slf4j.Logging
 
 
 /**
-  * case 1 -> sofisticated dependency injection use case
+  * case 1 -> dependency injection use case
   * note: the companion object receives it's own class implicit instance (Prefixer "injects" itself)
-
-final case class Prefixer(prefix: String)
 
 object Prefixer {
   implicit val p: Prefixer = Prefixer("***implicit***")
 }
+
+final case class Prefixer(prefix: String)
 
 class Executor {
   /* compiler scan the implicit Prefixer value here, on it's companion object */
@@ -33,29 +30,7 @@ object Test extends App {
 
 
 /**
-  * case 2 -> dangerous dependency injection use case
-  */
-final case class ConnectionFactory(table: String) {
-  def getConnection(implicit params: Map[String, String]): String = "connectionMock"
-}
-
-object Connecting extends App {
-
-  /*
-   * compiler wouldn't scan the implicit Map value on a ConnectionFactory companion object
-   * DANGER: any value passed here with type Map[String, String] is accepted as "params"
-   */
-  implicit val banana: Map[String, String] = Map.empty[String, String]
-
-  val factory = ConnectionFactory("TableMock")
-  val conn = factory.getConnection
-  print(conn + "connected ...")
-}
-
-
-/**
-  * case 3 ->
-  * note: the companion object receives it's own class implicit instance
+  * case 2 -> the same of case 1: the companion object receives it's own class implicit instance
   */
 final case class Blood(color: String)
 final case class User(name: String, blood: Blood)
@@ -65,12 +40,12 @@ final case class Beast(credentials: Credentials) {
   def rape(user:User): String = user.name + " rapped by " + credentials.id
 }
 
-final case class BeastVault(var beasts: Map[String, Beast])
-
 object BeastVault {
-  /* compiler scan the implicit reference value here, on it's companion OBJECT */
+  /* compiler scan the implicit reference value here, on BeastVault own companion OBJECT */
   implicit val beastVault: BeastVault = BeastVault(Map.empty[String, Beast])
 }
+
+final case class BeastVault(var beasts: Map[String, Beast])
 
 final case class Hell(implicit beastVault: BeastVault) {
 
@@ -113,23 +88,53 @@ object RapeTheUser extends App {
 
   Thread.sleep(1000)
 }
+
+
+/**
+  * case 3 -> simple dependency injection use case
+  */
+final case class ConnectionFactory(table: String) {
+
+  /* the implicit declared here does not work! */
+  // implicit val bananas: Map[String, String] = Map.empty[String, String]
+
+  def getConnection(implicit params: Map[String, String]): String = "connectionMock"
+}
+
+object ConnectionFactory {
+
+  /* the implicit declared here does not work too! It's not of type ConnectionFactory */
+  // implicit val bananas: Map[String, String] = Map.empty[String, String]
+
+  implicit val bananas: Map[String, String] = Map.empty[String, String]
+}
+
+object Connecting extends App {
+
+  /* DANGER ? any value passed as implicit here of type Map[String, String] is accepted as "params" */
+  implicit val bananas: Map[String, String] = Map.empty[String, String]
+
+  val factory = ConnectionFactory("TableMock")
+  val conn = factory.getConnection
+  print(conn + " connected ...")
+}
+
   */
 
 
-/*
-class Something(val someVar: Int)
-class Another(val anotherVar: Int)
 
-object Something {
-  implicit def qualquerPorra: Something = new Something(999)
-  implicit def outraPorra: Another = new Another(21)
-  implicit val bosta: Int = 29
+class Positives(val n: Int)
+class Negatives(val n: Int)
 
+object Positives {
+  implicit def one: Positives = new Positives(1)
+  implicit def two: Negatives = new Negatives(-2)
+  implicit val three: Int = 3
 }
 
 class SomeClass {
-  def someMethod(implicit someMoreImplicitVar:Something) = println(someMoreImplicitVar.someVar)
-  def anotherMethod(implicit anotherMoreImplicitVar:Another)= println(anotherMoreImplicitVar.anotherVar)
+  def calcSum(implicit positives:Positives): Int = positives.n + 5
+  def calcMultiply(implicit negatives:Negatives): Int= negatives.n * 5
 }
 
 trait M {
@@ -137,11 +142,11 @@ trait M {
   // says that "M cannot be mixed into a concrete class that does not also extend Method"
   this: SomeClass =>
 
-  implicit def x1 = new Something(11)
-  implicit def y1 = new Another(111)
+  implicit def eleven = new Positives(11)
+  implicit def twelve = new Negatives(-22)
 
-  def testy = anotherMethod
-  def testx = someMethod
+  def testy = calcSum
+  def testx = calcMultiply
 
 }
 
@@ -150,11 +155,11 @@ trait SM extends M {
 
   this: SomeClass =>
 
-  implicit def x2 = new Something(22)
-  implicit def y2 = new Another(222)
+  implicit def x2 = new Positives(22)
+  implicit def y2 = new Negatives(222)
 
-  def testy2 = anotherMethod
-  def testx2 = someMethod
+  def testy2 = :/
+  def testx2 = :\
 
 }
 
@@ -163,23 +168,23 @@ object TestThis extends App {
 
   // implicit resolved from companion object
   val theClassInstance = new SomeClass()
-  theClassInstance.someMethod
+  theClassInstance.:/
 
 
   // explicit applied so that value is used = 33
-  new SomeClass().someMethod(new Something(33))
+  new SomeClass().someMethod(new Positives(33))
 
   // will never use implicit inside M. implicit resolved from companion object of X = 1
   (new SomeClass with M).someMethod
 
   // local scope overrides companion object implicit = 30 and 30 again
-  implicit def x = new Something(30)
+  implicit def x = new Positives(30)
 
   new SomeClass().someMethod
   (new SomeClass with M).someMethod
 
   // again, explicit applied so that value is used = 5
-  new SomeClass().someMethod(new Something(5))
+  new SomeClass().someMethod(new Positives(5))
 
 
   /**
@@ -195,7 +200,7 @@ object TestThis extends App {
   // testx2 is within SM so the implicit within SM overrides the implicit in M and the companion object = 22
   (new SomeClass with SM).testx2
 }
-*/
+
 
 
 
